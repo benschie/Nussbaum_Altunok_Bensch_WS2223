b2e1df26102fd00543e851635e575076cd406369
benschie <64960303+benschie@users.noreply.github.com>
2023-01-12 13:09:04 +0100

    node.js usw. Update

--- node.js
+++ node.js
@@ -1,3 +1,8 @@
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+const $ = require('jquery')(window);
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
@@ -9,73 +14,115 @@ const app = express();
 app.use(cors({ origin: /http:\/\/localhost/ }));
 app.options('*', cors());
 
-// Erstellt eine Route, die auf '/events' reagiert
-app.get('/events', (req, res) => {
-  // Holt den Wert der query-Parameter 'city' aus der Anfrage
-  const city = req.query.city;
-
-  // Wenn 'city' nicht vorhanden ist, gib den HTML-Code zurück, der das Eingabefeld für den Benutzer anzeigt
-  if (!city) {
-
-    // Einfaches HTML Script, damit der Benutzer etwas auf der Seite sehen kann
-    return res.send(`
-      <h1>Events</h1>
-      <h5>Gloria, East Lansing, Hamden, Ann Arbor, Tuscaloosa, Madison, Iowa City, Las Vegas, Detroit oder New York eingeben</h5>
-      <form action="/events" method="GET">
-        <label for="city">Stadt:</label>
-        <input type="text" name="city" id="city" required>
-        <button type="submit">Events anzeigen</button>
-      </form>
-      <div id="events-container"></div>
-    `);
-  }
-
-  // Ansonsten mache eine API-Anfrage an die SeatGeek API und holt alle Events für die angegebene Stadt
-  axios
-  .get(`https://api.seatgeek.com/2/events?aid=123&venue.city=${city}`)
-  .then((response) => {
-    // Überprüft, ob es Events in der angegebenen Stadt gibt
-    if (response.data.events.length === 0) {
-      // Wenn es keine Events gibt, dann sende eine Nachricht zurück an den Benutzer, dass keine Events in der Stadt gefunden wurden
-      return res.send(`
-        <h1>Es gibt keine anstehenden Events in ${city}. Bitte geben Sie eine andere Stadt ein.</h1>
-        <h5>Gloria, East Lansing, Hamden, Ann Arbor, Tuscaloosa, Madison, Iowa City, Las Vegas, Detroit oder New York eingeben</h5>
-        <form action="/events" method="GET">
-          <label for="city">Stadt:</label>
-          <input type="text" name="city" id="city" required>
-          <button type="submit">Events anzeigen</button>
-        </form>
-      `);
+// Funktion, die die Hotelliste von der API abruft
+async function getHotels(city) {
+    try {
+      // Sendet eine Anfrage an die Hotellook API, um die Liste der Hotels für die angegebene Stadt abzurufen
+      const response = await axios.get(`https://engine.hotellook.com/api/v2/lookup.json?query=${city}`);
+      // Gibt die Liste der Hotels zurück
+      return response.data.hotels;
+    } catch (error) {
+      console.error(error);
     }
-    // Wenn es Events in der Stadt gibt, dann sende die Informationen der Events zurück an den Benutzer
-    res.send(`
-    <h1>Events in ${city}</h1>
-    ${response.data.events
-      .map((event) => {
-        // Geht jedes Event in der API-Antwort durch
-        // und erstellt ein HTML-Blockelement für jedes Event
-        return `
-        <details>
-          <summary>
-            <h2>${event.title}</h2>
-        </summary>
-        <p>Veranstaltungsort: ${event.venue.name}</p>
-        <p>Adresse: ${event.venue.address} ${event.venue.extended_address}</p>
-        <p>Datum: ${new Date(event.datetime_local).toLocaleDateString('de-DE')}</p>
-        <button onclick="getNearestHotel('${event.venue.location.lat},${event.venue.location.lon}', '${event.id}')">Nächstes Hotel anzeigen</button>
-        <div id="hotel-info-${event.id}"></div>
-        </details>
-            `;
-          })
-          // Fügt alle HTML-Elemente aus dem Array, das von der map()-Methode zurückgegeben wurde, 
-          // zu einem einzigen String zusammen. Die leere Zeichenkette wird als Separator verwendet, 
-          // sodass kein Separator zwischen den Elementen hinzugefügt wird. Das Ergebnis wird dann an den Browser gesendet (res.send()).
-          .join('')}
-      `);
+}
+
+function showHotels(city){
+    // Holt die Hotelliste von der API
+    getHotels(city).then((hotels) => {
+
+        // Iteriert durch die Hotelliste und erstelle HTML-Blockelemente für jedes Hotel
+        const hotelList = hotels.map((hotel) => {
+            return `<li>${hotel.results.hotels.lable}</li>`;
+        }).join('');
+        // Fügt die Hotelliste zur Seite hinzu
+        $("#hotel-container").html(`<ul>${hotelList}</ul>`);
     });
+}
+
+// Klick-Handler für den "Nächstes Hotel anzeigen"-Button
+$("#show-hotel-button").click(showHotels);
+
+
+async function getAvailableCities() {
+
+    try {
+
+      // Sendet eine Anfrage an die API, um die Liste der verfügbaren Städte abzurufen
+      const response = await axios.get('https://api.seatgeek.com/2/venues?aid=123');
+  
+      // Extrahiert die Namen der Städte aus der API-Antwort
+      const cityNames = response.data.venues.map((venue) => venue.city);
+  
+      // Gibt die Liste der Städtenamen zurück
+      return cityNames;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+
+app.get('/events', (req, res) => {
+    // Holt den Wert der query-Parameter 'city' aus der Anfrage
+    const city = req.query.city;
+  
+    // Wenn 'city' nicht vorhanden ist, gib den HTML-Code zurück, der das Eingabefeld für den Benutzer anzeigt
+    if (!city) {
+        // Lädt die Liste der verfügbaren Städte aus der API
+        getAvailableCities().then((availableCities) => {
+            // Wenn die Liste der verfügbaren Städte erfolgreich geladen wurde,
+            // generiere HTML-Code mit der Liste der Städte und sende ihn zurück an den Benutzer
+            return res.send(`
+              <h1>Events</h1>
+              <h5>Bitte wähle eine Stadt aus der Liste aus:</h5>
+              <form action="/events" method="GET">
+                <label for="city">Stadt:</label>
+                <select name="city" id="city" required>
+                  ${availableCities.map((city) => `<option value="${city}">${city}</option>`)}
+                </select>
+                <button type="submit">Events suchen</button>
+              </form>
+            `);
+        });
+    } else {
+        // Sendet eine Anfrage an die SeatGeek API, um die Liste der Events für die angegebene Stadt abzurufen
+        axios.get(`https://api.seatgeek.com/2/events?venue.city=${city}&aid=123`)
+            .then((response) => {
+                // Extrahiert die benötigten Daten aus der API-Antwort
+                const events = response.data.events;
+
+                const dropDownCity = $("#city").val();
+                // Wenn es Events gibt, generiere HTML-Code mit der Liste der Events und sende ihn zurück an den Benutzer
+                if (events.length > 0) {
+                    return res.send(`
+                        <h1>Events in ${city}</h1>
+                        ${events.map((event) => `
+                            <details>
+                              <summary>
+                                <h2>${event.title}</h2>
+                              </summary>
+                              <p>Veranstaltungsort: ${event.venue.name}</p>
+                              <p>Adresse: ${event.venue.address} ${event.venue.extended_address}</p>
+                              <p>Datum: ${new Date(event.datetime_local).toLocaleDateString()}</p>
+                              <p>Beginn: ${new Date(event.datetime_local).toLocaleTimeString()}</p>
+                              <p>Preis: ${event.stats.average_price ? `$${event.stats.average_price}` : 'Kostenlos'}</p>
+                              <a href="${event.url}" target="_blank">Weitere Informationen</a>
+                              <button id="show-hotel-button" onclick="${showHotels(city)}">Nächstes Hotel anzeigen</button>
+                            </details>
+                        `
+                    )})
+                .join('')}
+                `);
+            }
+        });
+    }
 });
 
+
+
+
+  
 // Startet den Server und gibt eine Meldung auf der Konsole aus, sobald der Server gestartet wurde
 const port = process.env.port || process.env.PORT || 1234;
 
 app.listen(port, () => console.log('Simple server running on http://localhost:1234'));
+
